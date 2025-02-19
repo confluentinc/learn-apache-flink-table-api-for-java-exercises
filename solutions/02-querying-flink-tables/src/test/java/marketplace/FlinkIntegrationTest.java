a18e02a3eaacf45cf531d395fe9b049266bb6bc7
Wade Waldron <wwaldron@confluent.io>
2025-02-19 13:50:45 -0600

    Upgrading the version of the Confluent plugin and switching to use DROP TABLE now that it is supported.

--- solutions/02-querying-flink-tables/src/test/java/marketplace/FlinkIntegrationTest.java
+++ solutions/02-querying-flink-tables/src/test/java/marketplace/FlinkIntegrationTest.java
@@ -2,33 +2,25 @@ package marketplace;
 
 import io.confluent.flink.plugin.ConfluentSettings;
 import org.apache.flink.table.api.EnvironmentSettings;
-import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
-import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
 import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.api.TableResult;
 import org.apache.flink.types.Row;
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.common.KafkaFuture;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.util.*;
-import java.util.function.Supplier;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public abstract class FlinkIntegrationTest {
     private List<TableResult> jobsToCancel;
-    private List<String> topicsToDelete;
+    private List<String> tablesToDelete;
     private Thread shutdownHook;
     private boolean isShuttingDown;
 
     protected TableEnvironment env;
-    protected AdminClient adminClient;
-    protected SchemaRegistryClient registryClient;
 
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -38,29 +30,10 @@ public abstract class FlinkIntegrationTest {
     @BeforeEach
     public void mainSetup() throws Exception {
         jobsToCancel = new ArrayList<>();
-        topicsToDelete = new ArrayList<>();
+        tablesToDelete = new ArrayList<>();
 
         EnvironmentSettings settings = ConfluentSettings.fromResource("/cloud.properties");
         env = TableEnvironment.create(settings);
-
-        Properties properties = new Properties();
-        settings.getConfiguration().toMap().forEach((k,v) ->
-            properties.put(k.replace("client.kafka.", ""), v)
-        );
-        adminClient = AdminClient.create(properties);
-
-        Map<String, String> schemaConfig = new HashMap<>();
-
-        schemaConfig.put("basic.auth.credentials.source", "USER_INFO");
-        schemaConfig.put(
-            "basic.auth.user.info",
-            properties.get("client.registry.key") + ":" + properties.get("client.registry.secret"));
-
-        registryClient = new CachedSchemaRegistryClient(
-            properties.getProperty("client.registry.url"),
-            100,
-            schemaConfig
-        );
 
         isShuttingDown = false;
         shutdownHook = new Thread(() -> {
@@ -84,9 +57,7 @@ public abstract class FlinkIntegrationTest {
                 .join()
         );
 
-        topicsToDelete.forEach(topic ->
-            deleteTopic(topic)
-        );
+        tablesToDelete.forEach(this::deleteTable);
 
         if(!isShuttingDown) {
             Runtime.getRuntime().removeShutdownHook(shutdownHook);
@@ -103,63 +74,29 @@ public abstract class FlinkIntegrationTest {
         return StreamSupport.stream(iterable.spliterator(), false);
     }
 
-    protected String getShortTableName(String tableName) {
-        String[] tablePath = tableName.split("\\.");
-        return tablePath[tablePath.length - 1].replace("`","");
-    }
-
-    protected void deleteTopic(String topicName) {
-        try {
-            String schemaName = topicName + "-value";
-            logger.info("Deleting Schema: "+schemaName);
-            if(registryClient.getAllSubjects().contains(schemaName)) {
-                registryClient.deleteSubject(schemaName, false);
-                registryClient.deleteSubject(schemaName, true);
-            }
-            logger.info("Deleted Schema: "+schemaName);
-        } catch (Exception e) {
-            logger.error("Error Deleting Schema", e);
-        }
-
-        try {
-            if(adminClient.listTopics().names().get().contains(topicName)) {
-                logger.info("Deleting Topic: " + topicName);
-                KafkaFuture<Void> result = adminClient.deleteTopics(List.of(topicName)).all();
-
-                while(!result.isDone()) {
-                    logger.info("Waiting for topic to be deleted: " + topicName);
-                    Thread.sleep(1000);
-                }
-
-                logger.info("Topic Deleted: " + topicName);
-            }
-        } catch (Exception e) {
-            logger.error("Error Deleting Topic", e);
-        }
-    }
-
     protected void deleteTable(String tableName) {
-        String topicName = getShortTableName(tableName);
-        deleteTopic(topicName);
-    }
+        String[] tablePath = tableName.split("\\.");
 
-    protected void deleteTopicOnExit(String topicName) {
-        topicsToDelete.add(topicName);
+        String catalog = tablePath[0].replace("`", "");
+        String database = tablePath[1].replace("`", "");
+        String table = tablePath[2].replace("`", "");
+
+        if(Arrays.asList(env.listTables(catalog, database)).contains(table)) {
+            logger.info("Deleting table {}", tableName);
+            env.executeSql(String.format("DROP TABLE %s", tableName));
+        }
     }
 
     protected void deleteTableOnExit(String tableName) {
-        String topicName = getShortTableName(tableName);
-        deleteTopicOnExit(topicName);
+        tablesToDelete.add(tableName);
     }
 
     protected void createTemporaryTable(String fullyQualifiedTableName, String tableDefinition) {
-        String topicName = getShortTableName(fullyQualifiedTableName);
-
         logger.info("Creating temporary table: " + fullyQualifiedTableName);
 
         try {
             env.executeSql(tableDefinition).await();
-            deleteTopicOnExit(topicName);
+            deleteTableOnExit(fullyQualifiedTableName);
 
             logger.info("Created temporary table: " + fullyQualifiedTableName);
         } catch (Exception e) {
